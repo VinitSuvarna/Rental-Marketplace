fda944866bbb112be7537850089e969aa2cffb47
Vinit Suvarna <vinitsuvarna182@gmail.com>
2025-11-24 23:20:03 +0530

    Serve media files (uploaded images) in production

--- rental_marketplace_project/urls.py
+++ rental_marketplace_project/urls.py
@@ -1,16 +1,15 @@
-# rental_marketplace_project/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings # 👈 IMPORT SETTINGS
-from django.conf.urls.static import static # 👈 IMPORT STATIC FUNCTION
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('core.urls')),  # 'name="index"' is likely here
-    path('items/', include('items.urls')),
-    path('users/', include('users.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("core.urls")),      # or whatever your main app is
+    path("items/", include("items.urls")),
+    path("rentals/", include("rentals.urls")),
+    path("users/", include("users.urls")),
 ]
 
-# 👇 ADD THIS BLOCK AT THE END OF THE FILE
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# 👇 ADD THIS at the end (outside the list)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
